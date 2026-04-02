87041e960e4ebf8d95953b2452dcea6aa75b6456
Percy Kuang <percykuang@foxmail.com>
2026-04-02 17:59:24 +0800

    feat(web): change chat interview quick prompt tech stack from Vue to React

--- packages/shared/src/constants/index.ts
+++ packages/shared/src/constants/index.ts
@@ -33,7 +33,7 @@ export const INTERVIEW_TOPICS: InterviewTopic[] = [
 
 export const QUICK_PROMPTS = [
   '可以帮我优化简历吗？',
-  '开始模拟面试，我是前端工程师 Vue 技术栈',
+  '开始模拟面试，我是前端工程师 React 技术栈',
   '前端面试时，如何正确的自我介绍',
   '我是前端工程师，如何挖掘简历项目亮点',
 ] as const;
